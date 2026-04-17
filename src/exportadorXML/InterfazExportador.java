d9bbe872f3a3a4b40bdad649a62a96ecf54df1bf
HodeiTorres33 <hodeitorres3@gmail.com>
2026-04-17 08:18:31 +0200

    cambio de nada en la interfaz del exportadorXML

--- src/exportadorXML/InterfazExportador.java
+++ src/exportadorXML/InterfazExportador.java
@@ -1,6 +1,17 @@
 package exportadorXML;
 
+import java.util.List;
+
+import model.Entrada;
+import model.Pelicula;
+import model.Sala;
+import model.Usuario;
+
 public interface InterfazExportador {
 
+    public void exportarCatalogo(List<Pelicula> listaPeliculas, List<Usuario>  listaUsuarios, 
+                                List<Entrada>  listaEntradas,
+                                 String ruta,
+                                 List<Sala> listaSalas);
 	
 }
